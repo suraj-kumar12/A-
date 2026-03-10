5e70e5a4f596c42c78ad565d58cb401b0621ea87
Suraj Kumar <surajroman2588@gmail.com>
2026-03-10 17:44:41 +0530

    3rd comment

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import React from 'react'
 
 const App = () => {
 
-  <style>
-   
-  </style>
   return (
     <div>
       <h1 className='text-red-700'> hello </h1>
-     <div className="img">
-      <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg" alt="" className='w-[200px]'/>
-     </div>
+     {/* <div className="img">
+      <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg" alt=""/>
+     </div> */}
     </div>
   )
 }
